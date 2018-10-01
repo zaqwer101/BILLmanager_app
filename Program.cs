f808874e72bcd6a4ac3b12d4a633c73296303a6b
zaqwer101 <platonov256@gmail.com>
2018-10-01 08:49:04 +0800

    Написал тестовую форму, отображающую тикеты

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace BILLmanager_app
@@ -16,6 +17,27 @@ namespace BILLmanager_app
         {
             AllTickets allTickets = BillmgrHandler.getTickets();
             Console.WriteLine(allTickets.Tickets[allTickets.Tickets.Length - 1].deadline); 
+            
+            Form mainForm = new Form();
+            ListView ticketsView = new ListView();
+            ticketsView.Dock = DockStyle.Fill;
+            ticketsView.View = View.Details;
+            ticketsView.FullRowSelect = true;
+            
+            
+            ticketsView.Columns.Add("ID", 100);
+            ticketsView.Columns.Add("Name", 300);
+            ticketsView.Columns.Add("Client", 300);
+            ticketsView.Columns.Add("Queue", 50);
+            ticketsView.Columns.Add("Deadline", 50);
+
+            string[] ticketInfo = { allTickets.Tickets[0].id, allTickets.Tickets[0].name, allTickets.Tickets[0].client, allTickets.Tickets[0].queue, allTickets.Tickets[0].deadline }; 
+            
+            var testItem = new ListViewItem(ticketInfo);
+            ticketsView.Items.Add(testItem);
+            
+            mainForm.Controls.Add(ticketsView);
+            mainForm.ShowDialog();
         }
 
         private static void ButtonHandler(object sender, EventArgs e)
